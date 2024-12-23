ec49b852d811fe91757a574a4b6943e5636a46e0
Pulkit <pulkitsinhaor@gmail.com>
2024-12-23 12:07:24 +0530

    no ssr pt2

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -1,10 +1,9 @@
 import dynamic from 'next/dynamic'
 const Grid = dynamic(() => import('@/components/Grid'), { ssr: false })
 const Hero = dynamic(() => import('@/components/Hero'), { ssr: false })
-const { TopNavbar } = dynamic(() => import('@/components/Navbar'), { ssr: false })
 // import Grid from '@/components/Grid'
 // import Hero from '@/components/Hero'
-// import { TopNavbar } from '@/components/Navbar'
+import { TopNavbar } from '@/components/Navbar'
 import Projects from '@/components/Projects'
 import React from 'react'
 
