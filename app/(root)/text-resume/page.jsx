33fe947458bc8d1f3c0cb0ced814ca83c4c1e7cd
Pulkit <pulkitsinhaor@gmail.com>
2024-12-26 03:01:33 +0530

    removed unnecessary images

---
+++ app/(root)/text-resume/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react'
+import { pdfjs } from 'react-pdf'
+
+const TextResume = () => {
+  return (
+    <div>
+      <h1>Resume</h1>
+    </div>
+  )
+}
+
+export default TextResume
